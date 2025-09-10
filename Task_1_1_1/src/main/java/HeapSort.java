1ee8a53c0d020df1b513c1f9cbf9107a23dec343
Marat Timofeev <m.timofeev1@g.nsu.ru>
2025-09-10 11:11:10 +0700

    Перелинтил код, добавил комментарии

--- Task_1_1_1/src/main/java/HeapSort.java
+++ Task_1_1_1/src/main/java/HeapSort.java
@@ -2,43 +2,56 @@
  * Классическая реализация пирамидальной сортировки (heapsort).
  */
 public class HeapSort {
+    /**
+     * Метод пирамидальной сортировки
+     * 
+     * @param arr
+     * @return отсортированный массив
+     */
+    public static int[] heapsort(int[] arr) {
+        int n = arr.length;
 
-	public static int[] heapsort(int[] arr) {
-		int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
 
-		for (int i = n / 2 - 1; i >= 0; i--) {
-			heapify(arr, n, i);
-		}
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
 
-		for (int i = n - 1; i > 0; i--) {
-			int temp = arr[0];
-			arr[0] = arr[i];
-			arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+        return arr;
+    }
 
-			heapify(arr, i, 0);
-		}
-		return arr;
-	}
+    /**
+     * Heapify - рекурсивная функция, которая перестраивает
+     * подмассив arr[rootIndex..heapSize-1] в пирамидальную форму.
+     * 
+     * @param arr       массив для сортировки
+     * @param heapSize  размер кучи
+     * @param rootIndex индекс вершины кучи
+     */
+    private static void heapify(int[] arr, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int left = 2 * rootIndex + 1;
+        int right = 2 * rootIndex + 2;
 
-	private static void heapify(int[] arr, int heapSize, int rootIndex) {
-		int largest = rootIndex;
-		int left = 2 * rootIndex + 1;
-		int right = 2 * rootIndex + 2;
+        if (left < heapSize && arr[left] > arr[largest]) {
+            largest = left;
+        }
 
-		if (left < heapSize && arr[left] > arr[largest]) {
-			largest = left;
-		}
+        if (right < heapSize && arr[right] > arr[largest]) {
+            largest = right;
+        }
 
-		if (right < heapSize && arr[right] > arr[largest]) {
-			largest = right;
-		}
+        if (largest != rootIndex) {
+            int swap = arr[rootIndex];
+            arr[rootIndex] = arr[largest];
+            arr[largest] = swap;
 
-		if (largest != rootIndex) {
-			int swap = arr[rootIndex];
-			arr[rootIndex] = arr[largest];
-			arr[largest] = swap;
-
-			heapify(arr, heapSize, largest);
-		}
-	}
+            heapify(arr, heapSize, largest);
+        }
+    }
 }
--- Task_1_1_1/src/main/java/Main.java
+++ Task_1_1_1/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
 
 public class Main {
+    /**
+     * Демонстрация работы пирамидальной сортировки.
+     */
     public static void main(String[] args) {
-        int[] input = {5, 4, 3, 2, 1};
+        int[] input = { 5, 4, 3, 2, 1 };
         int[] sorted = HeapSort.heapsort(input);
         System.out.println("Result: " + Arrays.toString(sorted));
     }
