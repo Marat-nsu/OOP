e186a2a95512b97344e313d4601f5b8079f9ac04
Marat Timofeev <m.timofeev1@g.nsu.ru>
2025-09-10 11:39:31 +0700

    lint mainTest

--- Task_1_1_1/src/test/java/MainTest.java
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
-	@Test
-	void testMain() {
-		String[] args = {};
-		Main.main(args);
-	}
+    @Test
+    void testMain() {
+        String[] args = {};
+        Main.main(args);
+    }
 }
