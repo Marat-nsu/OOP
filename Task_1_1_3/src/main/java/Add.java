e6e4b29da60ff3ff546d34caa108a83aa45bffe1
Marat Timofeev <m.timofeev1@g.nsu.ru>
2025-10-18 13:44:27 +0700

    lint

--- Task_1_1_3/src/main/java/Add.java
+++ Task_1_1_3/src/main/java/Add.java
@@ -1,32 +1,32 @@
 class Add extends Expression {
-	private Expression left, right;
+    private Expression left, right;
 
-	public Add(Expression left, Expression right) {
-		this.left = left;
-		this.right = right;
-	}
+    public Add(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
 
-	@Override
-	void print() {
-		System.out.print("(");
-		left.print();
-		System.out.print("+");
-		right.print();
-		System.out.print(")");
-	}
+    @Override
+    void print() {
+        System.out.print("(");
+        left.print();
+        System.out.print("+");
+        right.print();
+        System.out.print(")");
+    }
 
-	@Override
-	Expression derivative(String var) {
-		return new Add(left.derivative(var), right.derivative(var));
-	}
+    @Override
+    Expression derivative(String var) {
+        return new Add(left.derivative(var), right.derivative(var));
+    }
 
-	@Override
-	int eval(String assignments) {
-		return left.eval(assignments) + right.eval(assignments);
-	}
+    @Override
+    int eval(String assignments) {
+        return left.eval(assignments) + right.eval(assignments);
+    }
 
-	@Override
-	public String toString() {
-		return "(" + left.toString() + "+" + right.toString() + ")";
-	}
+    @Override
+    public String toString() {
+        return "(" + left.toString() + "+" + right.toString() + ")";
+    }
 }
--- Task_1_1_3/src/main/java/Number.java
+++ Task_1_1_3/src/main/java/Number.java
@@ -1,27 +1,27 @@
 class Number extends Expression {
-	private int value;
+    private int value;
 
-	public Number(int value) {
-		this.value = value;
-	}
+    public Number(int value) {
+        this.value = value;
+    }
 
-	@Override
-	void print() {
-		System.out.print(value);
-	}
+    @Override
+    void print() {
+        System.out.print(value);
+    }
 
-	@Override
-	Expression derivative(String var) {
-		return new Number(0);
-	}
+    @Override
+    Expression derivative(String var) {
+        return new Number(0);
+    }
 
-	@Override
-	int eval(String assignments) {
-		return value;
-	}
+    @Override
+    int eval(String assignments) {
+        return value;
+    }
 
-	@Override
-	public String toString() {
-		return String.valueOf(value);
-	}
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
 }
